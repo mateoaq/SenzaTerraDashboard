41e5c9895dc2f5f4805533d35dd4fede5fae7645
Mateo Aquino <57442760+mateoaq@users.noreply.github.com>
2022-03-27 15:03:12 -0300

    datos privados en .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
 import { getDatabase, ref, child, get } from "firebase/database";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBkfsQJdR7gAlpHMYEgYvnHSiQvmaKAgXU",
-    authDomain: "senza-terra.firebaseapp.com",
-    databaseURL: "https://senza-terra-default-rtdb.firebaseio.com",
-    projectId: "senza-terra",
-    storageBucket: "senza-terra.appspot.com",
-    messagingSenderId: "118016022623",
-    appId: "1:118016022623:web:f3deb2a49a0b8c415c63ea",
-    measurementId: "G-BVN9MN6D32"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
@@ -23,6 +22,13 @@ export const app = initializeApp(firebaseConfig);
 
 // Get a reference to the database service
 export const db = getDatabase(app);
+//FIREBASE_APPCHECK_DEBUG_TOKEN = '32AA835A - D972 - 43BA - A649 - AFCFC0D3F3B1'
+
+initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(process.env.REACT_APP_FIREBASE_APPCHECK_KEY),
+    isTokenAutoRefreshEnabled: true
+})
+
 
 const dbRef = ref(getDatabase());
 
